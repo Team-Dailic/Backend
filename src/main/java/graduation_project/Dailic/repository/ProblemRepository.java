eb4218d3fc38096637a5fd815db798003bf99e78
fivefar <fivefar@gmail.com>
2025-05-03 14:17:13 +0900

    Revert "Merge pull request #3 from Team-Dailic/origin/feat/#2-problem-entity"
    This reverts commit fc6f7b8378bd56f05abfc0162010f712d5c280ce, reversing
    changes made to 0cb79d866d11c19f3fa84df9e7ce09590ae9e86e.

--- src/main/java/graduation_project/Dailic/repository/ProblemRepository.java
+++
@@ -1,9 +0,0 @@
-package graduation_project.Dailic.repository;
-
-import graduation_project.Dailic.domain.Problem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProblemRepository extends JpaRepository<Problem, Long> {
-    // Custom query methods can be defined here if needed
-    // For example, find by question text or other attributes
-}
